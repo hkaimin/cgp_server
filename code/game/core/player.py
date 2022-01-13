09fc007d12fa91f6526cc8abb7859b2660d70a04
黄凯敏 <huangkaimin@huangkaimindeMacBook-Air.local>
2022-01-13 22:44:19 +0800

    contract2

--- code/game/core/player.py
+++ code/game/core/player.py
@@ -524,9 +524,11 @@ class Player(BasePlayer, netcmd.netCmd):
         pPro = subprocess.Popen(['sh','/root/contract/maincoin/contract.sh','%s'%self.data.account,'1','%s'%int(iAdd),'%s'%int(time.time()),'1'],stdout=subprocess.PIPE,shell=False,close_fds=True)
         #contractVal = os.system("sh /root/contract/maincoin/contract.sh %s %s %s %s %s"%(self.data.account,1,int(iAdd),int(time.time()),1))
         pPro.wait()
+        pPro2 = subprocess.Popen(['sh','/root/contract/maincoin/contract2.sh',"%s" % (pPro.stdout.readlines())],stdout=subprocess.PIPE,shell=False,close_fds=True)
+        pPro2.wait()
         self.base.setCoin(iAdd)
         self.markDirty()
-        Game.glog.log2File("contract", "%s" % (pPro.stdout.readlines()))
+        Game.glog.log2File("contract", "===pPro2=%s" % (pPro2.stdout.readlines()))
         return {"mainCoin":self.base.getCoin()}
 
     # 提取子币信息
