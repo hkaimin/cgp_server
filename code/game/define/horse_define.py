322211b8802b45f59f02baac3b650f0e1e6d506e
黄凯敏 <huangkaimin@huangkaimindeMacBook-Air.local>
2022-01-15 17:31:49 +0800

    player

--- code/game/define/horse_define.py
+++ code/game/define/horse_define.py
@@ -7,6 +7,6 @@ TICKET_COST_NUM  = 1 #门票价格消耗数量
 ENERGY_CONFIG = 100  #游戏体力上限
 HORSE_INFO = {
 	1:{
-		"iType":"s","strength":132,"MaxStrength":264,"speed":198,"MaxSpeed":396,""
+		"iType":"s","strength":132,"MaxStrength":264,"speed":198,"MaxSpeed":396
 	},
 }
