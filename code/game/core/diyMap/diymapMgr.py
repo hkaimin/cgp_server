4390811d982137b7aac31d90c2087f57916ada17
黄凯敏 <huangkaimin@huangkaimindeMacBook-Air.local>
2022-03-19 01:34:32 +0800

    merge

--- code/game/core/diyMap/diymapMgr.py
+++ code/game/core/diyMap/diymapMgr.py
@@ -237,7 +237,9 @@ class DiyMapInfo(utility.DirtyFlag):
 
         self.markDirty()
         self.data.save(Game.store, forced=True, no_let=True)
-        return {"iType":dLoad["iType"],"res_key":dLoad["res_key"],"star":dLoad["star"],"name":dLoad["name"]}
+        return {"iType":horse_define.HORSE_INFO[dLoad["iRandomHorseType"]]["iType"]
+                ,"res_key":horse_define.HORSE_INFO[dLoad["iRandomHorseType"]]["res_key"]
+                ,"star":dLoad["star"],"name":dLoad["name"]}
 
     def getRandomMax(self,maxNum,tRandomSub):
         return int(random.randint(tRandomSub[0],tRandomSub[1])/100.0 * maxNum)
